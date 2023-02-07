0bee18b082921e75b2ad7b3ef9cf377f18b61bdf
Joseph Chan <josephchan36@gmail.com>
2023-02-07 12:01:34 -0800

    Updated views

--- app/src/main/java/com/example/josephchan_lab5/MainActivity.kt
+++ app/src/main/java/com/example/josephchan_lab5/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.josephchan_lab5
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    fun swapFragments(v: View){
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentContainer_main, FighterFragment())
+        fragmentTransaction.commit()
+    }
+
+
 }
